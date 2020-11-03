aaa2a2e9a3a25da209150c5d96693522de33b276
tuyentac95 <tuyentac95@gmail.com>
2020-11-03 10:37:17 +0700

    thêm product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {LoginComponent} from './login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { AdminComponent } from './admin/admin.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
